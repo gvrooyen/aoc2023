7f04e1b6eb154b3ce39e83b2c5c8cb839a6e99f4
G-J van Rooyen <g-j@octoco.ltd>
2023-12-03 16:39:22 +0200

    Implemented basic number extraction for Day 3

--- 03/lib/Aoc.ml
+++ 03/lib/Aoc.ml
@@ -4,12 +4,29 @@ let title = "AoC 2023 - Day ?"
 
 (* PART 1 *)
 
-let foo (filename : string) =
-  In_channel.with_file filename ~f:(fun file ->
-    In_channel.fold_lines file ~init:0 ~f:(fun acc line ->
-      acc + String.length(line)
-    )
-  )
+let pull_number ~idx s =
+  let c = String.get s idx in
+  if not (Char.is_digit c) then None
+  else
+    let rec seek_left i w =
+      if i < 0 then w
+      else
+        let c' = String.get s i in
+        if Char.is_digit c' then seek_left (i - 1) ((Char.to_string c') ^ w)
+        else w
+    in
+    let rec seek_right i w =
+      if i >= String.length s then w
+      else
+        let c' = String.get s i in
+        if Char.is_digit c' then seek_right (i + 1) (w ^ (Char.to_string c')) 
+        else w
+    in
+    let w' = (seek_left (idx - 1) (Char.to_string c) |> seek_right (idx + 1)) in
+    Some (int_of_string w')
+
+let read_schematic (filename : string) =
+  In_channel.with_file filename ~f:In_channel.input_line
 
 (* PART 2 *)
 
--- 03/test/test_aoc.ml
+++ 03/test/test_aoc.ml
@@ -1,9 +1,14 @@
 open! Core
 open Aoc
 
-let%test_unit "test_name" =
-  [%test_eq: int] (1 + 1) 2
+let%test_unit "pull_number Some x" =
+  [%test_eq: int option] 
+    (pull_number "..123...." ~idx:3) (Some 123)
+
+let%test_unit "pull_number None" =
+  [%test_eq: int option]
+    (pull_number "..123...." ~idx:5) None
 
 let%expect_test _ =
   print_s [%sexp (1 + 1 : int)];
-  [%expect {||}]
+  [%expect {| 2 |}]
