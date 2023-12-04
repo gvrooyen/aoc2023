01a8d38955981f859f1a86ab6d855cb9a5d3741c
G-J van Rooyen <g-j@octoco.ltd>
2023-12-04 08:00:42 +0200

    Add part number extraction around a symbol

--- 03/lib/Aoc.ml
+++ 03/lib/Aoc.ml
@@ -4,29 +4,64 @@ let title = "AoC 2023 - Day ?"
 
 (* PART 1 *)
 
-let pull_number ~idx s =
-  let c = String.get s idx in
-  if not (Char.is_digit c) then None
-  else
-    let rec seek_left i w =
-      if i < 0 then w
+type schematic = string list
+
+type symbol = {
+  line: int;
+  col: int;
+} [@@deriving sexp]
+
+let pull_number ~idx s' =
+  if (Option.is_none s') || (idx < 0) then None
+  else 
+    let s = Option.value_exn s' in
+    if idx >= (String.length s) then None
+    else
+      let c = String.get s idx in
+      if not (Char.is_digit c) then None
       else
-        let c' = String.get s i in
-        if Char.is_digit c' then seek_left (i - 1) ((Char.to_string c') ^ w)
-        else w
-    in
-    let rec seek_right i w =
-      if i >= String.length s then w
-      else
-        let c' = String.get s i in
-        if Char.is_digit c' then seek_right (i + 1) (w ^ (Char.to_string c')) 
-        else w
-    in
-    let w' = (seek_left (idx - 1) (Char.to_string c) |> seek_right (idx + 1)) in
-    Some (int_of_string w')
+        let rec seek_left i w =
+          if i < 0 then w
+          else
+            let c' = String.get s i in
+            if Char.is_digit c' then seek_left (i - 1) ((Char.to_string c') ^ w)
+            else w
+        in
+        let rec seek_right i w =
+          if i >= String.length s then w
+          else
+            let c' = String.get s i in
+            if Char.is_digit c' then seek_right (i + 1) (w ^ (Char.to_string c')) 
+            else w
+        in
+        let w' = (seek_left (idx - 1) (Char.to_string c) |> seek_right (idx + 1)) in
+        Some (int_of_string w')
 
 let read_schematic (filename : string) =
-  In_channel.with_file filename ~f:In_channel.input_line
+  In_channel.with_file filename ~f:(fun file ->
+    In_channel.fold_lines file ~init:[] ~f:(fun acc line ->
+      line :: acc
+    ) |> List.rev
+  )
+
+let find_symbols (s : schematic) =
+  List.foldi s ~init:[] ~f:(fun i acc line ->
+    String.foldi line ~init:acc ~f:(fun j acc c ->
+      match c with
+        | '.' -> acc
+        | c' when Char.is_digit c' -> acc
+        | _ -> { line = i; col = j } :: acc
+    )
+  ) |> List.rev
+
+let find_partnums s line col =
+  let dirs = [(-1, -1); (-1, 0); (-1, 1);
+    (0, -1); (0, 1);
+    (1, -1); (1, 0); (1, 1)] in
+  List.map dirs ~f:(fun (dy, dx) ->
+    pull_number (List.nth s (line + dy)) ~idx:(col + dx)
+  ) |> List.filter_map ~f:Fn.id
+    (* |> List.dedup_and_sort ~compare:Int.compare *)
 
 (* PART 2 *)
 
--- 03/test/test_aoc.ml
+++ 03/test/test_aoc.ml
@@ -3,12 +3,34 @@ open Aoc
 
 let%test_unit "pull_number Some x" =
   [%test_eq: int option] 
-    (pull_number "..123...." ~idx:3) (Some 123)
+    (pull_number (Some "..123....") ~idx:3) (Some 123)
 
 let%test_unit "pull_number None" =
   [%test_eq: int option]
-    (pull_number "..123...." ~idx:5) None
+    (pull_number (Some "..123....") ~idx:5) None
+
+let%test_unit "pull_number neg idx" =
+  [%test_eq: int option]
+    (pull_number (Some "..123....") ~idx:(-1)) None
+
+let%test_unit "pull_number oob idx" =
+  [%test_eq: int option]
+    (pull_number (Some "..123....") ~idx:(100)) None
+
+let%test_unit "pull_number None string" =
+  [%test_eq: int option]
+    (pull_number None ~idx:0) None
+
+let s = read_schematic "test.txt"
 
 let%expect_test _ =
-  print_s [%sexp (1 + 1 : int)];
-  [%expect {| 2 |}]
+  let syms = find_symbols s in
+  print_s [%sexp (syms : symbol list)];
+  [%expect {|
+    (((line 1) (col 3)) ((line 3) (col 6)) ((line 4) (col 3)) ((line 5) (col 5))
+     ((line 8) (col 3)) ((line 8) (col 5))) |}]
+
+let%expect_test _ =
+  let partnums = find_partnums s 1 3 in
+  print_s [%sexp (partnums : int list)];
+  [%expect {||}]
