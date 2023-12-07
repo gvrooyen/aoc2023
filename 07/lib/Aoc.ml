3a1a827044af30da3c9a306581484cfa7b396303
G-J van Rooyen <g-j@octoco.ltd>
2023-12-07 17:34:23 +0200

    Added card counting and hand classification logic

--- 07/lib/Aoc.ml
+++ 07/lib/Aoc.ml
@@ -1,8 +1,15 @@
 open! Core
 
-let title = "AoC 2023 - Day ?"
+let title = "AoC 2023 - Day 7"
 
 (* PART 1 *)
+
+let cards = ['2'; '3'; '4'; '5'; '6'; '7';
+  '8'; '9'; 'T'; 'J'; 'Q'; 'K'; 'A']
+
+let card_rank c =
+  cards |> List.findi ~f:(fun _ c' -> Char.equal c c')
+  |> Option.value_exn |> fst
 
 let foo (filename : string) =
   In_channel.with_file filename ~f:(fun file ->
@@ -10,6 +17,34 @@ let foo (filename : string) =
       acc + String.length(line)
     )
   )
+
+type hand_type = K5 | K4 | FH | K3 | PP | P | Z
+  [@@deriving sexp, compare]
+
+type card_count = (char * int) list
+  [@@deriving sexp, compare]
+
+let count_cards s : card_count =
+  if not (String.length s = 5) then failwith "invalid hand length"
+  else
+    String.fold s ~init:[] ~f:(fun acc c ->
+      match List.Assoc.find acc ~equal:Char.equal c with
+        | Some n -> List.Assoc.add acc ~equal:Char.equal c (n + 1)
+        | None -> List.Assoc.add acc ~equal:Char.equal c 1
+    )
+    |> List.sort ~compare:(fun (_, n1) (_, n2) ->
+          Int.compare n2 n1
+    )
+
+let hand_type_of_string s =
+    match count_cards s with
+      | [(_, 5)] -> K5
+      |  (_, 4)  :: _ -> K4
+      | [(_, 3); (_, 2)] -> FH
+      |  (_, 3)  :: _ -> K3
+      | [(_, 2); (_, 2); _] -> PP
+      |  (_, 2) :: _ -> P
+      | _ -> Z
 
 (* PART 2 *)
 
--- 07/test/test_aoc.ml
+++ 07/test/test_aoc.ml
@@ -1,9 +1,23 @@
 open! Core
 open! Aoc
 
-let%test_unit "test_name" =
-  [%test_eq: int] (1 + 1) 2
+let%test_unit "card_rank" =
+  [%test_eq: int] (card_rank '2') 0;
+  [%test_eq: int] (card_rank '7') 5;
+  [%test_eq: int] (card_rank 'A') 12
 
-let%expect_test _ =
-  print_s [%sexp (1 + 1 : int)];
-  [%expect {||}]
+let%test_unit "card_count" =
+  [%test_eq: card_count] (count_cards "AA3A3") [('A', 3); ('3', 2)]
+
+let%test_unit "hand_type" =
+  [%test_eq: hand_type] (hand_type_of_string "AAAAA") K5;
+  [%test_eq: hand_type] (hand_type_of_string "A2AAA") K4;
+  [%test_eq: hand_type] (hand_type_of_string "AA3A3") FH;
+  [%test_eq: hand_type] (hand_type_of_string "A4AA5") K3;
+  [%test_eq: hand_type] (hand_type_of_string "A6A62") PP;
+  [%test_eq: hand_type] (hand_type_of_string "A78A9") P;
+  [%test_eq: hand_type] (hand_type_of_string "AKJQ2") Z
+
+(* let%expect_test _ = *)
+(*   print_s [%sexp (1 + 1 : int)]; *)
+(*   [%expect {||}] *)
